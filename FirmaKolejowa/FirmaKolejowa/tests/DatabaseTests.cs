a6493d4f99ddd1f0bcb179eff6c9e8b433309f14
david799 <43647970+david799@users.noreply.github.com>
2022-01-19 21:07:20 +0100

    User database operations tests added

--- FirmaKolejowa/FirmaKolejowa/tests/DatabaseTests.cs
+++ FirmaKolejowa/FirmaKolejowa/tests/DatabaseTests.cs
@@ -51,5 +51,45 @@ namespace FirmaKolejowa.Testy
             var trains = _database.getTrains();
             Assert.Greater(_database.deleteTrain(trains[trains.Count - 1].id), 0);
         }
+
+        [Test]
+        public void UserAdding()
+        {
+            var user = new User("nick", "password", "name", "surname");
+            Assert.Greater(_database.addUser(user), 0);
+            var users = _database.getUsers();
+            _database.deleteTrain(users[users.Count - 1].id);
+        }
+
+        [Test]
+        public void UsersGetting()
+        {
+            var user = new User("nick", "password", "name", "surname");
+            _database.addUser(user);
+            var users = _database.getUsers();
+            Assert.Greater(users.Count, 0);
+            _database.deleteTrain(users[users.Count - 1].id);
+        }
+
+        [Test]
+        public void UserEditing()
+        {
+            var user = new User("nick", "password", "name", "surname");
+            _database.addUser(user);
+            var users = _database.getUsers();
+            var lastUser = users[users.Count - 1];
+            lastUser.name = "updatedName";
+            Assert.Greater(_database.updateUser(lastUser), 0);
+            _database.deleteUser(lastUser.id);
+        }
+
+        [Test]
+        public void UserDeleting()
+        {
+            var user = new User("nick", "password", "name", "surname");
+            _database.addUser(user);
+            var users = _database.getUsers();
+            Assert.Greater(_database.deleteUser(users[users.Count - 1].id), 0);
+        }
     }
 }
