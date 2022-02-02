a173dc5625fa793dec89cbae6a7aac693b39bc45
Nevaan <pawel.losek@outlook.com>
2022-02-02 18:09:03 +0100

    Activating/deactivating train - buttons

---
+++ FirmaKolejowa/FirmaKolejowa/Model/AdminTrainListActivationModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace FirmaKolejowa.Model
+{
+    public class AdminTrainListActivationModel: INotifyPropertyChanged
+    {
+        private bool canActivate;
+        private bool canDeactivate;
+
+        public bool CanActivate
+        {
+            get { return canActivate; }
+            set
+            {
+                canActivate = value;
+                OnPropertyChanged("CanActivate");
+            }
+        }
+
+
+        public bool CanDeactivate
+        {
+            get { return canDeactivate; }
+            set
+            {
+                canDeactivate = value;
+                OnPropertyChanged("CanDeactivate");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- FirmaKolejowa/FirmaKolejowa/ViewModels/Navigable/AdminTrainListViewModel.cs
+++ FirmaKolejowa/FirmaKolejowa/ViewModels/Navigable/AdminTrainListViewModel.cs
@@ -9,8 +9,16 @@ namespace FirmaKolejowa.ViewModels
     class AdminTrainListViewModel : NavigableViewModel
     {
         private ObservableCollection<AdminTrainListElementModel> _trains = new ObservableCollection<AdminTrainListElementModel>();
+        private AdminTrainListElementModel selectedTrain;
+        private AdminTrainListActivationModel _adminTrainListActivationModel = new AdminTrainListActivationModel() { CanActivate = false, CanDeactivate = false };
 
         public ObservableCollection<AdminTrainListElementModel> Trains { get { return _trains; } }
+        public AdminTrainListElementModel SelectedTrain { get { return selectedTrain; } set { 
+                selectedTrain = value;
+                ActivateModel.CanActivate = value != null && !value.IsActive;
+                ActivateModel.CanDeactivate = value != null && value.IsActive;
+            } }
+        public AdminTrainListActivationModel ActivateModel { get { return _adminTrainListActivationModel; } set { _adminTrainListActivationModel = value; } }
 
         private ICompanyDatabase _database;
         public ICompanyDatabase database { get { return _database; } }
