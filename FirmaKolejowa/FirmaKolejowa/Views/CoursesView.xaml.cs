c16004200c1558aed20942233bc7e830d9a4e8aa
david799 <43647970+david799@users.noreply.github.com>
2022-02-03 18:35:21 +0100

    Course canceling added

--- FirmaKolejowa/FirmaKolejowa/Views/CoursesView.xaml.cs
+++ FirmaKolejowa/FirmaKolejowa/Views/CoursesView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BackendFirmaKolejowa.Configuration;
+using BackendFirmaKolejowa.db.repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,25 @@ namespace FirmaKolejowa.Views
         public CoursesView()
         {
             InitializeComponent();
+        }
+
+        public void CancelButtonClicked(object sender, RoutedEventArgs e)
+        {
+            DbConfig dbConfig = new DbConfig();
+            var _database = dbConfig.getCompanyDatabase();
+            var course_id = int.Parse(textBox.Text);
+            var course = _database.getCourse(course_id);
+            if (course == null) return;
+            course.canceled = true;
+            _database.updateCourse(course);
+            var tickets = _database.getTickets();
+            var thisCourseTickets = tickets.Where(ticket => ticket.course_id == course_id);
+            foreach (var ticket in thisCourseTickets)
+            {
+                ticket.status = 0; // 0 means canceled
+                _database.updateTicket(ticket);
+            }
+            (sender as Button).Command.Execute(null);
         }
     }
 }
